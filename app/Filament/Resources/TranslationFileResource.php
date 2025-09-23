078a2e0b210ca76481ebbe3b54075299ca6eeaf3
marco76tv <marco.sottana@gmail.com>
2025-09-23 17:18:57 +0200

    .

--- app/Filament/Resources/TranslationFileResource.php
+++ app/Filament/Resources/TranslationFileResource.php
@@ -9,7 +9,25 @@ use Illuminate\Support\Facades\Config;
 use Modules\Lang\Filament\Resources\TranslationFileResource\Pages;
 use Modules\Lang\Filament\Resources\TranslationFileResource\Pages\CreateTranslationFile;
 use Modules\Lang\Filament\Resources\TranslationFileResource\Pages\EditTranslationFile;
+<<<<<<< HEAD
 use Modules\Lang\Filament\Resources\TranslationFileResource\Pages\ListTranslationFiles;
+=======
+use Filament\Actions;
+use Filament\Forms\Components;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\Facades\Config;
+use Modules\Lang\Filament\Actions\LocaleSwitcherRefresh;
+use Modules\Lang\Filament\Resources\TranslationFileResource\Pages;
+use Modules\Lang\Filament\Resources\TranslationFileResource\RelationManagers;
+>>>>>>> f64d66e (.)
 use Modules\Lang\Models\TranslationFile;
 use Modules\Xot\Filament\Resources\XotBaseResource;
 use Override;
