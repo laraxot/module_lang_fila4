ab049abf898e1a74f4558c5698a96b886ea7c1c6
marco76tv <marco.sottana@gmail.com>
2025-09-30 21:18:57 +0200

    .

--- app/Filament/Forms/Components/NationalFlagSelect.php
+++ app/Filament/Forms/Components/NationalFlagSelect.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Modules\Lang\Filament\Forms\Components;
+namespace Modules\Notify\Filament\Forms\Components;
 
 use Filament\Forms\Components\Select;
 use Illuminate\Support\Arr;
