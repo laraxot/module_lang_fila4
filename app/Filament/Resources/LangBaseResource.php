09409e27c37fd813e960fa0971f4093cb700eaba
marco76tv <marco.sottana@gmail.com>
2025-10-22 09:28:04 +0200

    .

--- app/Filament/Resources/LangBaseResource.php
+++ app/Filament/Resources/LangBaseResource.php
@@ -3,6 +3,12 @@
 declare(strict_types=1);
 
 namespace Modules\Lang\Filament\Resources;
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+>>>>>>> a7ee0d6 (.)
 
 // use LaraZeus\SpatieTranslatable\Resources\Concerns\Translatable; // Temporaneamente commentato per compatibilità Filament 4.x
 use Illuminate\Support\Facades\Config;
@@ -22,4 +28,65 @@ abstract class LangBaseResource extends XotBaseResource
     // {
     //     return ['it', 'en'];
     // }
+<<<<<<< HEAD
+=======
+=======
+use LaraZeus\SpatieTranslatable\Resources\Concerns\Translatable;
+=======
+
+// use LaraZeus\SpatieTranslatable\Resources\Concerns\Translatable; // Temporaneamente commentato per compatibilità Filament 4.x
+>>>>>>> b93ef594b4 (.)
+use Filament\Actions;
+use Filament\Resources\Pages\EditRecord;
+use Illuminate\Support\Facades\Config;
+use Modules\Cms\Filament\Resources\SectionResource;
+use Modules\Xot\Filament\Resources\Pages\XotBaseEditRecord;
+use Modules\Xot\Filament\Resources\XotBaseResource;
+
+abstract class LangBaseResource extends XotBaseResource
+{
+    // use Translatable; // Temporaneamente commentato per compatibilità Filament 4.x
+
+    // Temporaneamente commentato per compatibilità Filament 4.x
+    // public static function getDefaultTranslatableLocale(): string
+    // {
+    //     return Config::string('app.locale', 'it');
+    // }
+
+<<<<<<< HEAD
+>>>>>>> a12f125f4a (.)
+=======
+    // public static function getTranslatableLocales(): array
+    // {
+    //     return ['it', 'en'];
+    // }
+>>>>>>> b93ef594b4 (.)
+=======
+use Filament\Actions;
+use Illuminate\Support\Facades\Config;
+use Filament\Resources\Pages\EditRecord;
+use Filament\Resources\Concerns\Translatable;
+use Modules\Cms\Filament\Resources\SectionResource;
+use Modules\Xot\Filament\Resources\XotBaseResource;
+use Modules\Xot\Filament\Resources\Pages\XotBaseEditRecord;
+
+abstract class LangBaseResource extends XotBaseResource
+{
+    use Translatable;
+
+
+    public static function getDefaultTranslatableLocale(): string
+    {
+        return Config::string('app.locale', 'it');
+    }
+
+    public static function getTranslatableLocales(): array
+    {
+        return ['it', 'en'];
+    }
+
+
+
+>>>>>>> origin/develop
+>>>>>>> a7ee0d6 (.)
 }
--- app/Filament/Resources/Pages/LangBaseEditRecord.php
+++ app/Filament/Resources/Pages/LangBaseEditRecord.php
@@ -3,22 +3,79 @@
 declare(strict_types=1);
 
 namespace Modules\Lang\Filament\Resources\Pages;
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+>>>>>>> a7ee0d6 (.)
 
 use LaraZeus\SpatieTranslatable\Actions\LocaleSwitcher;
 use LaraZeus\SpatieTranslatable\Resources\Pages\EditRecord\Concerns\Translatable;
 use Modules\Cms\Filament\Resources\SectionResource;
+<<<<<<< HEAD
+=======
+=======
+=======
+
+>>>>>>> b93ef594b4 (.)
+use LaraZeus\SpatieTranslatable\Resources\Pages\EditRecord\Concerns\Translatable;
+use LaraZeus\SpatieTranslatable\Actions\LocaleSwitcher;
+use Filament\Actions;
+use Filament\Resources\Pages\EditRecord;
+<<<<<<< HEAD
+>>>>>>> a12f125f4a (.)
+=======
+use Modules\Cms\Filament\Resources\SectionResource;
+>>>>>>> b93ef594b4 (.)
+=======
+use Filament\Actions;
+use Modules\Cms\Filament\Resources\SectionResource;
+use Filament\Resources\Pages\EditRecord;
+>>>>>>> origin/develop
+>>>>>>> a7ee0d6 (.)
 use Modules\Xot\Filament\Resources\Pages\XotBaseEditRecord;
 
 abstract class LangBaseEditRecord extends XotBaseEditRecord
 {
+<<<<<<< HEAD
     protected static string $resource; // = SectionResource::class;
 
     // use Translatable; // Temporaneamente commentato per compatibilità Filament 4.x
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+    protected static string $resource; // = SectionResource::class;
+
+    // use Translatable; // Temporaneamente commentato per compatibilità Filament 4.x
+=======
+    protected static string $resource;// = SectionResource::class;
+    use Translatable;
+>>>>>>> a12f125f4a (.)
+=======
+    protected static string $resource; // = SectionResource::class;
+
+    // use Translatable; // Temporaneamente commentato per compatibilità Filament 4.x
+>>>>>>> b93ef594b4 (.)
+=======
+    protected static string $resource;// = SectionResource::class;
+    use EditRecord\Concerns\Translatable;
+>>>>>>> origin/develop
+>>>>>>> a7ee0d6 (.)
 
     protected function getHeaderActions(): array
     {
         return [
+<<<<<<< HEAD
             LocaleSwitcher::make(),
+=======
+<<<<<<< HEAD
+            LocaleSwitcher::make(),
+=======
+            Actions\LocaleSwitcher::make(),
+>>>>>>> origin/develop
+>>>>>>> a7ee0d6 (.)
             ...parent::getHeaderActions(),
             // ...
         ];
