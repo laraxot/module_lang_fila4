3d90973a3fde7c321f61a2e069bc4e86e7880036
marco76tv <marco.sottana@gmail.com>
2025-10-22 09:27:40 +0200

    .

--- app/Models/Policies/TranslationFilePolicy.php
+++ app/Models/Policies/TranslationFilePolicy.php
@@ -5,63 +5,109 @@ declare(strict_types=1);
 namespace Modules\Lang\Models\Policies;
 
 use Modules\Lang\Models\TranslationFile;
+<<<<<<< HEAD
 use Modules\Xot\Contracts\UserContract;
+=======
+use Modules\Xot\Contracts\ProfileContract;
+>>>>>>> 8b0b6ac (.)
 
 class TranslationFilePolicy extends LangBasePolicy
 {
     /**
      * Determine whether the user can view any models.
      */
+<<<<<<< HEAD
     public function viewAny(UserContract $user): bool
     {
         return $user->hasPermissionTo('translation_file.viewAny');
+=======
+    public function viewAny(ProfileContract $user): bool
+    {
+        return $user->hasPermissionTo('translation_file.viewAny'); /** @phpstan-ignore method.nonObject */
+>>>>>>> 8b0b6ac (.)
     }
 
     /**
      * Determine whether the user can view the model.
      */
+<<<<<<< HEAD
     public function view(UserContract $user, TranslationFile $_translation_file): bool
     {
         return $user->hasPermissionTo('translation_file.view');
+=======
+    public function view(ProfileContract $user, TranslationFile $translation_file): bool
+    {
+        return $user->hasPermissionTo('translation_file.view'); /** @phpstan-ignore method.nonObject */
+>>>>>>> 8b0b6ac (.)
     }
 
     /**
      * Determine whether the user can create models.
      */
+<<<<<<< HEAD
     public function create(UserContract $user): bool
     {
         return $user->hasPermissionTo('translation_file.create');
+=======
+    public function create(ProfileContract $user): bool
+    {
+        return $user->hasPermissionTo('translation_file.create'); /** @phpstan-ignore method.nonObject */
+>>>>>>> 8b0b6ac (.)
     }
 
     /**
      * Determine whether the user can update the model.
      */
+<<<<<<< HEAD
     public function update(UserContract $user, TranslationFile $_translation_file): bool
     {
         return $user->hasPermissionTo('translation_file.update');
+=======
+    public function update(ProfileContract $user, TranslationFile $translation_file): bool
+    {
+        return $user->hasPermissionTo('translation_file.update'); /** @phpstan-ignore method.nonObject */
+>>>>>>> 8b0b6ac (.)
     }
 
     /**
      * Determine whether the user can delete the model.
      */
+<<<<<<< HEAD
     public function delete(UserContract $user, TranslationFile $_translation_file): bool
     {
         return $user->hasPermissionTo('translation_file.delete');
+=======
+    public function delete(ProfileContract $user, TranslationFile $translation_file): bool
+    {
+        return $user->hasPermissionTo('translation_file.delete'); /** @phpstan-ignore method.nonObject */
+>>>>>>> 8b0b6ac (.)
     }
 
     /**
      * Determine whether the user can restore the model.
      */
+<<<<<<< HEAD
     public function restore(UserContract $user, TranslationFile $_translation_file): bool
     {
         return $user->hasPermissionTo('translation_file.restore');
+=======
+    public function restore(ProfileContract $user, TranslationFile $translation_file): bool
+    {
+        return $user->hasPermissionTo('translation_file.restore'); /** @phpstan-ignore method.nonObject */
+>>>>>>> 8b0b6ac (.)
     }
 
     /**
      * Determine whether the user can permanently delete the model.
      */
+<<<<<<< HEAD
     public function forceDelete(UserContract $user, TranslationFile $translation_file): bool
     {
         return $user->hasPermissionTo('translation_file.forceDelete');
+=======
+    public function forceDelete(ProfileContract $user, TranslationFile $translation_file): bool
+    {
+        return $user->hasPermissionTo('translation_file.forceDelete'); /** @phpstan-ignore method.nonObject */
+>>>>>>> 8b0b6ac (.)
     }
 }
--- app/Models/Translation.php
+++ app/Models/Translation.php
@@ -116,6 +116,7 @@ class Translation extends BaseModel
      * Get the current connection name for the model.
      *
      * @return string|null
+<<<<<<< HEAD
      *
      * public function getConnectionName()
      * {
@@ -126,4 +127,16 @@ class Translation extends BaseModel
      * return parent::getConnectionName();
      * }
      */
+=======
+
+    public function getConnectionName()
+    {
+        if ($connection = config('translation-manager.db_connection')) {
+            return $connection;
+        }
+
+        return parent::getConnectionName();
+    }
+    */
+>>>>>>> 8b0b6ac (.)
 }
