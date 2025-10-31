9033de6261b165336a50e08304451fffbacbb487
marco76tv <marco.sottana@gmail.com>
2025-10-31 13:00:55 +0100

    .

--- app/Actions/GetTransPathAction.php
+++ app/Actions/GetTransPathAction.php
@@ -30,7 +30,7 @@ class GetTransPathAction
             $lang_path = base_path('Modules/'.$ns.'/lang');
         }
         $file_name = $piece[0] ?? '';
-        Assert::string($file_name, 'Il nome del file deve essere una stringa');
+        // $file_name is always string due to ?? ''
 
         return $lang_path.'/'.$lang.'/'.$file_name.'.php';
     }
