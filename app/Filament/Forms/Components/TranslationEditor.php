61d87fc4048283ba1acb7757f86cc7678751c1b2
marco76tv <marco.sottana@gmail.com>
2025-11-04 10:23:02 +0100

    .

--- app/Filament/Forms/Components/TranslationEditor.php
+++ app/Filament/Forms/Components/TranslationEditor.php
@@ -18,15 +18,12 @@ class TranslationEditor extends Field
     {
         parent::setUp();
 
-        $this->afterStateHydrated(function (TranslationEditor $component, $state): void {
+        $this->afterStateHydrated(function (TranslationEditor $component, $state) {
             $component->state($state ?? []);
         });
     }
 
-    /**
-     * @return array<string, mixed>
-     */
-    public function getDefaultChildComponents(): array
+    public function getDefaultChildComponents(?string $key = null): array
     {
         $components = [];
         $state = $this->getState() ?? [];
@@ -35,15 +32,12 @@ class TranslationEditor extends Field
         }
 
         foreach ($state as $key => $value) {
-            $keyStr = is_string($key) ? $key : (string) $key;
-            
             if (is_array($value)) {
-                $components[$keyStr] = Section::make($keyStr)->schema([
-                    TranslationEditor::make($keyStr)->label('')->state($value),
+                $components[] = Section::make($key)->schema([
+                    TranslationEditor::make($key)->label('')->state($value),
                 ]);
             } else {
-                $valueStr = is_string($value) ? $value : (string) $value;
-                $components[$keyStr] = TextInput::make($keyStr)->label(str_replace('_', ' ', $keyStr))->default($valueStr);
+                $components[] = TextInput::make($key)->label(str_replace('_', ' ', $key))->default($value);
             }
         }
 
