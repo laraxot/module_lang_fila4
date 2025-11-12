e3aca73f46bff16e649da34b900daed49a4886b6
marco76tv <marco.sottana@gmail.com>
2025-11-12 15:09:25 +0100

    .

--- app/Models/BaseModel.php
+++ app/Models/BaseModel.php
@@ -66,10 +66,30 @@ abstract class BaseModel extends Model
     {
         return [
             'id' => 'string',
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> b93ef594b4 (.)
+>>>>>>> a7ee0d6 (.)
             'uuid' => 'string',
             'published_at' => 'datetime',
             'created_at' => 'datetime',
             'updated_at' => 'datetime',
         ];
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+=======
+            'uuid' => 'string', 'published_at' => 'datetime', 'created_at' => 'datetime', 'updated_at' => 'datetime'];
+>>>>>>> a12f125f4a (.)
+=======
+>>>>>>> b93ef594b4 (.)
+=======
+            'uuid' => 'string', 'published_at' => 'datetime', 'created_at' => 'datetime', 'updated_at' => 'datetime'];
+>>>>>>> origin/develop
+>>>>>>> a7ee0d6 (.)
     }
 }
