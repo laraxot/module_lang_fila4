b6a3f2af94ca4fa08b4aa863dcd2147bb37e93bc
marco76tv <marco.sottana@gmail.com>
2025-11-12 15:10:39 +0100

    .

--- app/Datas/TranslationData.php
+++ app/Datas/TranslationData.php
@@ -46,6 +46,9 @@ class TranslationData extends Data
         return $this->filename;
     }
 
+    /**
+     * @return array<string, mixed>
+     */
     public function getData(): array
     {
         $filename = $this->getFilename();
