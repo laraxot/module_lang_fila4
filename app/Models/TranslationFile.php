1174f4619e5ce20b61eeefbb714ce9b99247164e
marco76tv <marco.sottana@gmail.com>
2025-11-12 11:08:05 +0100

    .

--- app/Models/TranslationFile.php
+++ app/Models/TranslationFile.php
@@ -48,11 +48,7 @@ class TranslationFile extends BaseModel
         'content',
     ];
 
-<<<<<<< HEAD
     protected array $form = [
-=======
-    protected array $schema = [
->>>>>>> f5327e8 (.)
         'key' => 'string',
         'path' => 'string',
         'id' => 'string',
