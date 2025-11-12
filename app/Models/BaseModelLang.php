d10b08c1c6f0207c9ea18c9f8664e6fad5b9081d
marco76tv <marco.sottana@gmail.com>
2025-11-12 10:02:28 +0100

    .

--- app/Models/BaseModelLang.php
+++ app/Models/BaseModelLang.php
@@ -63,10 +63,35 @@ abstract class BaseModelLang extends Model
 
     // -----------
     /*
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> b93ef594b4 (.)
+>>>>>>> a7ee0d6 (.)
      * protected $id;
      * protected $post;
      * protected $lang;
      */
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+=======
+=======
+>>>>>>> origin/develop
+    protected $id;
+    protected $post;
+    protected $lang;
+    */
+<<<<<<< HEAD
+>>>>>>> a12f125f4a (.)
+=======
+>>>>>>> b93ef594b4 (.)
+=======
+>>>>>>> origin/develop
+>>>>>>> a7ee0d6 (.)
     /**
      * Create a new factory instance for the model.
      *
@@ -83,10 +108,30 @@ abstract class BaseModelLang extends Model
     {
         return [
             'id' => 'string',
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> b93ef594b4 (.)
+>>>>>>> a7ee0d6 (.)
             'uuid' => 'string',
             'published_at' => 'datetime',
             'created_at' => 'datetime',
             'updated_at' => 'datetime',
         ];
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+=======
+            'uuid' => 'string', 'published_at' => 'datetime', 'created_at' => 'datetime', 'updated_at' => 'datetime'];
+>>>>>>> a12f125f4a (.)
+=======
+>>>>>>> b93ef594b4 (.)
+=======
+            'uuid' => 'string', 'published_at' => 'datetime', 'created_at' => 'datetime', 'updated_at' => 'datetime'];
+>>>>>>> origin/develop
+>>>>>>> a7ee0d6 (.)
     }
 }
