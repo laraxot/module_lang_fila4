90d3a58e7e786cb51efc715ef0bd248bf409fd9a
marco76tv <marco.sottana@gmail.com>
2025-11-12 15:10:12 +0100

    .

--- app/Providers/Traits/TranslatorTrait.php
+++ app/Providers/Traits/TranslatorTrait.php
@@ -13,44 +13,11 @@ trait TranslatorTrait
     public function registerTranslator(): void
     {
         // Override the JSON Translator
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> a7ee0d6 (.)
         $this->app->extend('translator', static function (Translator $translator): TranslatorService {
             $translatorService = new TranslatorService($translator->getLoader(), $translator->getLocale());
             $translatorService->setFallback($translator->getFallback());
 
             return $translatorService;
         });
-<<<<<<< HEAD
-=======
-=======
-=======
->>>>>>> origin/develop
-        $this->app->extend(
-            'translator',
-            static function (Translator $translator): TranslatorService {
-                $translatorService = new TranslatorService($translator->getLoader(), $translator->getLocale());
-                $translatorService->setFallback($translator->getFallback());
-
-                return $translatorService;
-            }
-        );
-<<<<<<< HEAD
->>>>>>> a12f125f4a (.)
-=======
-        $this->app->extend('translator', static function (Translator $translator): TranslatorService {
-            $translatorService = new TranslatorService($translator->getLoader(), $translator->getLocale());
-            $translatorService->setFallback($translator->getFallback());
-
-            return $translatorService;
-        });
->>>>>>> b93ef594b4 (.)
-=======
->>>>>>> origin/develop
->>>>>>> a7ee0d6 (.)
     }
 }
