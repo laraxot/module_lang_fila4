b202b82e1c86fd9081afe64b9983af6d07038085
marco76tv <marco.sottana@gmail.com>
2025-11-12 10:02:22 +0100

    .

--- app/Actions/Filament/AutoLabelAction.php
+++ app/Actions/Filament/AutoLabelAction.php
@@ -105,7 +105,11 @@ class AutoLabelAction
 
         /*
          * $component_parent=get_parent_class($component);
+<<<<<<< HEAD
          * if(in_array($component_parent,['Filament\Schemas\Components\Component'])){
+=======
+         * if(in_array($component_parent,['Filament\Forms\Components\Component'])){
+>>>>>>> a1de4f9 (.)
          * $component_key=Str::of(class_basename($component))->plural()->snake()->toString();
          * }else{
          * $component_key=Str::of(class_basename($component_parent))->plural()->snake()->toString();
