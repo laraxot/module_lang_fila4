d40eec5a96c0043f7cd8c1867d8f9cd41e2fb5af
marco76tv <marco.sottana@gmail.com>
2025-11-12 10:02:49 +0100

    .

--- app/Filament/Resources/TranslationFileResource.php
+++ app/Filament/Resources/TranslationFileResource.php
@@ -13,36 +13,10 @@ use Modules\Lang\Filament\Resources\TranslationFileResource\Pages\ListTranslatio
 use Modules\Lang\Models\TranslationFile;
 use Modules\Xot\Filament\Resources\XotBaseResource;
 use Override;
-<<<<<<< HEAD
 
 class TranslationFileResource extends XotBaseResource
 {
     protected static ?string $model = TranslationFile::class;
-=======
-use Modules\Lang\Filament\Resources\TranslationFileResource\Pages\ListTranslationFiles;
-use Modules\Lang\Filament\Resources\TranslationFileResource\Pages\CreateTranslationFile;
-use Modules\Lang\Filament\Resources\TranslationFileResource\Pages\EditTranslationFile;
-use Filament\Actions;
-use Filament\Forms\Components;
-use Filament\Forms\Components\Section;
-use Filament\Forms\Components\TextInput;
-use Filament\Schemas\Schema;
-use Filament\Resources\Resource;
-use Filament\Tables;
-use Filament\Tables\Table;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
-use Illuminate\Support\Facades\Config;
-use Modules\Lang\Filament\Actions\LocaleSwitcherRefresh;
-use Modules\Lang\Filament\Resources\TranslationFileResource\Pages;
-use Modules\Lang\Filament\Resources\TranslationFileResource\RelationManagers;
-use Modules\Lang\Models\TranslationFile;
-use Modules\Xot\Filament\Resources\XotBaseResource;
-
-class TranslationFileResource extends XotBaseResource
-{
-    protected static null|string $model = TranslationFile::class;
->>>>>>> a87590b (.)
 
     public static function getDefaultTranslatableLocale(): string
     {
