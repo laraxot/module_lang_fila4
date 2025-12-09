1314d4675462352c31fec3fcc0936f289afa553a
marco76tv <marco.sottana@gmail.com>
2025-12-09 10:54:16 +0100

    .

--- lang/it/txt.php
+++ lang/it/txt.php
@@ -1,6 +1,9 @@
 <?php
 
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 1c4a063 (.)
 declare(strict_types=1);
 
 return [
@@ -474,6 +477,7 @@ return [
         ],
     ],
 ];
+<<<<<<< HEAD
 =======
 return array (
   'fields' => 
@@ -1023,3 +1027,5 @@ return array (
   ),
 );
 >>>>>>> 8b0b6ac (.)
+=======
+>>>>>>> 1c4a063 (.)
