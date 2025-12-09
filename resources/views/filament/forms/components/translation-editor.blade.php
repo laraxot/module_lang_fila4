de46289df5003860951a66a3ca8036a8c6734c02
marco76tv <marco.sottana@gmail.com>
2025-12-09 16:52:36 +0100

    .

--- resources/views/filament/forms/components/translation-editor.blade.php
+++ resources/views/filament/forms/components/translation-editor.blade.php
@@ -1,3 +1,11 @@
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> b93ef594b4 (.)
+>>>>>>> a7ee0d6 (.)
 <?php
 
 declare(strict_types=1);
@@ -7,3 +15,21 @@ declare(strict_types=1);
 <div>
     {{ $this->getChildComponents() }}
 </div>
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+=======
+<!-- resources/views/filament/components/translation-editor.blade.php -->
+<div>
+    {{ $this->getChildComponents() }}
+</div>
+>>>>>>> a12f125f4a (.)
+=======
+>>>>>>> b93ef594b4 (.)
+=======
+<!-- resources/views/filament/components/translation-editor.blade.php -->
+<div>
+    {{ $this->getChildComponents() }}
+</div>
+>>>>>>> origin/develop
+>>>>>>> a7ee0d6 (.)
