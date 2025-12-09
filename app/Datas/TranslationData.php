07cc452a278bee77829e5e323a0f91e9d0c774b3
marco76tv <marco.sottana@gmail.com>
2025-12-09 16:55:46 +0100

    .

--- app/Datas/TranslationData.php
+++ app/Datas/TranslationData.php
@@ -45,6 +45,9 @@ class TranslationData extends Data
         return $this->filename;
     }
 
+    /**
+     * @return array<string, mixed>
+     */
     public function getData(): array
     {
         $filename = $this->getFilename();
@@ -56,6 +59,7 @@ class TranslationData extends Data
             throw new \Exception('['.__LINE__.']['.class_basename($this).']');
         }
 
+        /** @var array<string, mixed> */
         return $data;
     }
 }
