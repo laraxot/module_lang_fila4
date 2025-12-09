4d9d5ac290ee819cdfbcf408d89d83c6483cfc38
marco76tv <marco.sottana@gmail.com>
2025-12-10 00:04:27 +0100

    .

--- app/Filament/Resources/TranslationFileResource/Pages/ListTranslationFiles.php
+++ app/Filament/Resources/TranslationFileResource/Pages/ListTranslationFiles.php
@@ -9,6 +9,7 @@ use Filament\Tables\Columns\TextColumn;
 use Modules\Lang\Filament\Actions\LocaleSwitcherRefresh;
 use Modules\Lang\Filament\Resources\TranslationFileResource;
 use Modules\Xot\Filament\Resources\Pages\XotBaseListRecords;
+use Override;
 
 class ListTranslationFiles extends XotBaseListRecords
 {
