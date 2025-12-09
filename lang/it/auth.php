0a73e1db6d853102bd506b052dbd4f72d574f94d
marco76tv <marco.sottana@gmail.com>
2025-12-09 16:46:41 +0100

    .

--- lang/it/auth.php
+++ lang/it/auth.php
@@ -1,11 +1,5 @@
 <?php
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> a7ee0d6 (.)
 declare(strict_types=1);
 
 return [
@@ -19,61 +13,10 @@ return [
      * | these language lines according to your application's requirements.
      * |
      */
-<<<<<<< HEAD
-=======
-=======
-=======
->>>>>>> origin/develop
-return [
-    /*
-    |--------------------------------------------------------------------------
-    | Authentication Language Lines
-    |--------------------------------------------------------------------------
-    |
-    | The following language lines are used during authentication for various
-    | messages that we need to display to the user. You are free to modify
-    | these language lines according to your application's requirements.
-    |
-    */
-<<<<<<< HEAD
->>>>>>> a12f125f4a (.)
-=======
-declare(strict_types=1);
-
-
-return [
-    /*
-     * |--------------------------------------------------------------------------
-     * | Authentication Language Lines
-     * |--------------------------------------------------------------------------
-     * |
-     * | The following language lines are used during authentication for various
-     * | messages that we need to display to the user. You are free to modify
-     * | these language lines according to your application's requirements.
-     * |
-     */
->>>>>>> b93ef594b4 (.)
-=======
->>>>>>> origin/develop
->>>>>>> a7ee0d6 (.)
 
     'failed' => 'Credenziali non valide.',
     'password' => 'La password fornita non è corretta.',
     'throttle' => 'Troppi tentativi di accesso. Riprova tra :seconds secondi.',
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-
->>>>>>> a12f125f4a (.)
-=======
->>>>>>> b93ef594b4 (.)
-=======
-
->>>>>>> origin/develop
->>>>>>> a7ee0d6 (.)
     'login' => [
         'title' => 'Accedi',
         'email' => 'Email',
@@ -85,20 +28,6 @@ return [
         'create_account' => 'crea un nuovo account',
         'link' => 'Accedi',
     ],
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-
->>>>>>> a12f125f4a (.)
-=======
->>>>>>> b93ef594b4 (.)
-=======
-
->>>>>>> origin/develop
->>>>>>> a7ee0d6 (.)
     'register' => [
         'title' => 'Registati',
         'name' => 'Nome',
@@ -109,59 +38,17 @@ return [
         'already_registered' => 'Hai già un account?',
         'link' => 'Registrati',
     ],
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-
->>>>>>> a12f125f4a (.)
-=======
->>>>>>> b93ef594b4 (.)
-=======
-
->>>>>>> origin/develop
->>>>>>> a7ee0d6 (.)
     'verify' => [
         'title' => 'Verifica il tuo indirizzo email',
         'success' => 'Un nuovo link di verifica è stato inviato al tuo indirizzo email.',
         'notice' => 'Prima di procedere, controlla la tua email per un link di verifica. Se non hai ricevuto l\'email,',
         'another_request' => 'clicca qui per richiederne un\'altra',
     ],
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-
->>>>>>> a12f125f4a (.)
-=======
->>>>>>> b93ef594b4 (.)
-=======
-
->>>>>>> origin/develop
->>>>>>> a7ee0d6 (.)
     'forgot_password' => [
         'title' => 'Password dimenticata',
         'email' => 'Email',
         'submit' => 'Invia link di reset',
     ],
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-
->>>>>>> a12f125f4a (.)
-=======
->>>>>>> b93ef594b4 (.)
-=======
-
->>>>>>> origin/develop
->>>>>>> a7ee0d6 (.)
     'reset_password' => [
         'title' => 'Reimposta la password',
         'email' => 'Email',
@@ -169,39 +56,11 @@ return [
         'password_confirmation' => 'Conferma Password',
         'submit' => 'Reimposta Password',
     ],
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-
->>>>>>> a12f125f4a (.)
-=======
->>>>>>> b93ef594b4 (.)
-=======
-
->>>>>>> origin/develop
->>>>>>> a7ee0d6 (.)
     'confirm_password' => [
         'title' => 'Conferma la password',
         'password' => 'Password',
         'submit' => 'Conferma',
     ],
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-
->>>>>>> a12f125f4a (.)
-=======
->>>>>>> b93ef594b4 (.)
-=======
-
->>>>>>> origin/develop
->>>>>>> a7ee0d6 (.)
     'logout' => [
         'submit' => 'Logout',
         'title' => 'Logout',
@@ -216,20 +75,6 @@ return [
         'try_again' => 'Riprova',
         'processing' => 'Disconnessione in corso...',
     ],
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-    
->>>>>>> a12f125f4a (.)
-=======
->>>>>>> b93ef594b4 (.)
-=======
-    
->>>>>>> origin/develop
->>>>>>> a7ee0d6 (.)
     'user_dropdown' => [
         'manage_account' => 'Gestisci Account',
         'profile' => 'Profilo',
