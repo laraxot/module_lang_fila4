7a9b6dd672ac92534ddd053ed42e19173293cc9c
marco76tv <marco.sottana@gmail.com>
2025-12-10 00:06:50 +0100

    .

--- lang/it/auth.php
+++ lang/it/auth.php
@@ -1,5 +1,6 @@
 <?php
 
+<<<<<<< HEAD
 declare(strict_types=1);
 
 return [
@@ -13,10 +14,27 @@ return [
      * | these language lines according to your application's requirements.
      * |
      */
+=======
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+>>>>>>> 8b0b6ac (.)
 
     'failed' => 'Credenziali non valide.',
     'password' => 'La password fornita non è corretta.',
     'throttle' => 'Troppi tentativi di accesso. Riprova tra :seconds secondi.',
+<<<<<<< HEAD
+=======
+
+>>>>>>> 8b0b6ac (.)
     'login' => [
         'title' => 'Accedi',
         'email' => 'Email',
@@ -28,6 +46,10 @@ return [
         'create_account' => 'crea un nuovo account',
         'link' => 'Accedi',
     ],
+<<<<<<< HEAD
+=======
+
+>>>>>>> 8b0b6ac (.)
     'register' => [
         'title' => 'Registati',
         'name' => 'Nome',
@@ -38,17 +60,29 @@ return [
         'already_registered' => 'Hai già un account?',
         'link' => 'Registrati',
     ],
+<<<<<<< HEAD
+=======
+
+>>>>>>> 8b0b6ac (.)
     'verify' => [
         'title' => 'Verifica il tuo indirizzo email',
         'success' => 'Un nuovo link di verifica è stato inviato al tuo indirizzo email.',
         'notice' => 'Prima di procedere, controlla la tua email per un link di verifica. Se non hai ricevuto l\'email,',
         'another_request' => 'clicca qui per richiederne un\'altra',
     ],
+<<<<<<< HEAD
+=======
+
+>>>>>>> 8b0b6ac (.)
     'forgot_password' => [
         'title' => 'Password dimenticata',
         'email' => 'Email',
         'submit' => 'Invia link di reset',
     ],
+<<<<<<< HEAD
+=======
+
+>>>>>>> 8b0b6ac (.)
     'reset_password' => [
         'title' => 'Reimposta la password',
         'email' => 'Email',
@@ -56,11 +90,19 @@ return [
         'password_confirmation' => 'Conferma Password',
         'submit' => 'Reimposta Password',
     ],
+<<<<<<< HEAD
+=======
+
+>>>>>>> 8b0b6ac (.)
     'confirm_password' => [
         'title' => 'Conferma la password',
         'password' => 'Password',
         'submit' => 'Conferma',
     ],
+<<<<<<< HEAD
+=======
+
+>>>>>>> 8b0b6ac (.)
     'logout' => [
         'submit' => 'Logout',
         'title' => 'Logout',
@@ -75,6 +117,10 @@ return [
         'try_again' => 'Riprova',
         'processing' => 'Disconnessione in corso...',
     ],
+<<<<<<< HEAD
+=======
+    
+>>>>>>> 8b0b6ac (.)
     'user_dropdown' => [
         'manage_account' => 'Gestisci Account',
         'profile' => 'Profilo',
