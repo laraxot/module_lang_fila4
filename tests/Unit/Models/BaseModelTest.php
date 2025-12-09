2bf808b603744dc8b95b5fb501b5ec9d7c726403
marco76tv <marco.sottana@gmail.com>
2025-12-09 16:41:17 +0100

    .

--- tests/Unit/Models/BaseModelTest.php
+++ tests/Unit/Models/BaseModelTest.php
@@ -3,6 +3,9 @@
 declare(strict_types=1);
 
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 1c4a063 (.)
 namespace Modules\Lang\Tests\Unit\Models;
 
 use Illuminate\Database\Eloquent\Model;
@@ -14,6 +17,7 @@ uses(TestCase::class, RefreshDatabase::class);
 
 beforeEach(function () {
     $this->baseModel = new class extends BaseModel {
+<<<<<<< HEAD
 =======
 use Illuminate\Database\Eloquent\Model;
 use Modules\Lang\Models\BaseModel;
@@ -22,6 +26,8 @@ beforeEach(function () {
     $this->baseModel = new class extends BaseModel
     {
 >>>>>>> 8b0b6ac (.)
+=======
+>>>>>>> 1c4a063 (.)
         protected $table = 'test_lang_table';
     };
 });
