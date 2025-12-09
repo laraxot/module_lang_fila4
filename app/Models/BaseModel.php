aacb994fd09c30f6ffc32ece0db24b8d83a7b185
marco76tv <marco.sottana@gmail.com>
2025-12-10 00:07:31 +0100

    .

--- app/Models/BaseModel.php
+++ app/Models/BaseModel.php
@@ -23,10 +23,14 @@ abstract class BaseModel extends XotBaseModel
     {
         return [
             'id' => 'string',
+<<<<<<< HEAD
             'uuid' => 'string',
             'published_at' => 'datetime',
             'created_at' => 'datetime',
             'updated_at' => 'datetime',
         ];
+=======
+            'uuid' => 'string', 'published_at' => 'datetime', 'created_at' => 'datetime', 'updated_at' => 'datetime'];
+>>>>>>> 8b0b6ac (.)
     }
 }
