9ce799eda66481581b556930f993c752c05bee35
marco76tv <marco76tv@users.noreply.github.com>
2025-12-09 23:19:13 +0000

    Check & fix styling

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -7,7 +7,6 @@ namespace Modules\Lang\Providers\Filament;
 use Filament\Panel;
 use LaraZeus\SpatieTranslatable\SpatieTranslatablePlugin;
 use Modules\Xot\Providers\Filament\XotBasePanelProvider;
-use Override;
 
 class AdminPanelProvider extends XotBasePanelProvider
 {
