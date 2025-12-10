a69d1957450d31993f46205d5ea842b8a63cc247
marco76tv <marco.sottana@gmail.com>
2025-12-10 11:39:12 +0100

    .

--- lang/it/locale_switcher_refresh.php
+++ lang/it/locale_switcher_refresh.php
@@ -1,6 +1,9 @@
 <?php
 
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 1c4a063 (.)
 declare(strict_types=1);
 
 return [
@@ -13,6 +16,7 @@ return [
         ],
     ],
 ];
+<<<<<<< HEAD
 =======
 return array (
   'fields' => 
@@ -27,3 +31,5 @@ return array (
   ),
 );
 >>>>>>> 8b0b6ac (.)
+=======
+>>>>>>> 1c4a063 (.)
