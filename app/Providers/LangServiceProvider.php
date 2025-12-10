f884d05334faa145728392c8270d88f5ea8f26bd
marco76tv <marco76tv@users.noreply.github.com>
2025-12-10 20:39:36 +0000

    Check & fix styling

--- app/Providers/LangServiceProvider.php
+++ app/Providers/LangServiceProvider.php
@@ -58,7 +58,7 @@ class LangServiceProvider extends XotBaseServiceProvider
             Assert::isInstanceOf($component, Field::class);
 
             $validationMessages = __('user::validation');
-            if (is_array($validationMessages) && $validationMessages !== []) {
+            if (is_array($validationMessages) && [] !== $validationMessages) {
                 /** @var array<string, string> $typedMessages */
                 $typedMessages = [];
                 foreach ($validationMessages as $key => $value) {
@@ -67,7 +67,7 @@ class LangServiceProvider extends XotBaseServiceProvider
                     }
                 }
 
-                if ($typedMessages !== []) {
+                if ([] !== $typedMessages) {
                     $component->validationMessages($typedMessages);
                 }
             }
