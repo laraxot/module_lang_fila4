41f8ade6643f65d4063ff9597dc02b67b28b3294
marco76tv <marco.sottana@gmail.com>
2025-12-10 18:04:11 +0100

    .

--- resources/views/filament/widgets/language-switcher.blade.php
+++ resources/views/filament/widgets/language-switcher.blade.php
@@ -1,17 +1,8 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 1c4a063 (.)
 <?php
 
 declare(strict_types=1);
 
 ?>
-<<<<<<< HEAD
-=======
->>>>>>> 8b0b6ac (.)
-=======
->>>>>>> 1c4a063 (.)
 {{--
 /**
  * Language Switcher Widget
@@ -229,12 +220,4 @@ if (!function_exists('getLanguageUrl')) {
     }
 }
 @endphp
-<<<<<<< HEAD
-<<<<<<< HEAD
 </div>
-=======
-</div>
->>>>>>> 8b0b6ac (.)
-=======
-</div>
->>>>>>> 1c4a063 (.)
