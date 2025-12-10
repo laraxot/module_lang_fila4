85d12c7f04ec86a7ee750325e74ce8432f485df4
marco76tv <marco.sottana@gmail.com>
2025-12-10 11:16:33 +0100

    .

--- lang/it/translation_editor.php
+++ lang/it/translation_editor.php
@@ -1,13 +1,5 @@
 <?php
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> b93ef594b4 (.)
->>>>>>> a7ee0d6 (.)
 declare(strict_types=1);
 
 return [
@@ -20,28 +12,3 @@ return [
         ],
     ],
 ];
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-=======
->>>>>>> origin/develop
-return array (
-  'fields' => 
-  array (
-    'fields' => 
-    array (
-      'label' => 'fields',
-      'placeholder' => 'fields',
-      'helper_text' => 'fields',
-      'description' => 'fields',
-    ),
-  ),
-);
-<<<<<<< HEAD
->>>>>>> a12f125f4a (.)
-=======
->>>>>>> b93ef594b4 (.)
-=======
->>>>>>> origin/develop
->>>>>>> a7ee0d6 (.)
