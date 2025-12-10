06456ef5297632e0b6c1420bd24ea42ed4fd21a5
marco76tv <marco.sottana@gmail.com>
2025-12-10 11:44:40 +0100

    .

--- app/Datas/LangData.php
+++ app/Datas/LangData.php
@@ -38,7 +38,6 @@ class LangData extends Data
      * Crea una collezione di dati di lingua.
      *
      * @param  EloquentCollection<int, mixed>|Collection<int, mixed>|array<int, mixed>  $data
-     *
      * @return DataCollection<LangData>
      */
     public static function collection(EloquentCollection|Collection|array $data): DataCollection
