6060b5b575a12b6630d96da7c602615bab590442
marco76tv <marco.sottana@gmail.com>
2025-12-10 11:38:16 +0100

    .

--- lang/it/locale_switcher_refresh.php
+++ lang/it/locale_switcher_refresh.php
@@ -1,5 +1,13 @@
 <?php
 
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> b93ef594b4 (.)
+>>>>>>> a7ee0d6 (.)
 declare(strict_types=1);
 
 return [
@@ -12,3 +20,28 @@ return [
         ],
     ],
 ];
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+=======
+=======
+>>>>>>> origin/develop
+return array (
+  'fields' => 
+  array (
+    'locale' => 
+    array (
+      'label' => 'locale',
+      'placeholder' => 'locale',
+      'helper_text' => 'locale',
+      'description' => 'locale',
+    ),
+  ),
+);
+<<<<<<< HEAD
+>>>>>>> a12f125f4a (.)
+=======
+>>>>>>> b93ef594b4 (.)
+=======
+>>>>>>> origin/develop
+>>>>>>> a7ee0d6 (.)
