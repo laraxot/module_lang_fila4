66209e0d121e45ea67c5917c43f8c7eeb769e2a8
marco76tv <marco.sottana@gmail.com>
2025-12-10 17:54:21 +0100

    .

--- resources/views/components/flag.blade.php
+++ resources/views/components/flag.blade.php
@@ -1,6 +1,24 @@
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> b93ef594b4 (.)
+>>>>>>> a7ee0d6 (.)
 <?php
 
 declare(strict_types=1);
 
 ?>
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+=======
+>>>>>>> a12f125f4a (.)
+=======
+>>>>>>> b93ef594b4 (.)
+=======
+>>>>>>> origin/develop
+>>>>>>> a7ee0d6 (.)
 <img src="{{ Theme::asset('lang::svg/it.svg') }}" width="50px" />
