a56095e9209e3735522776ccdab39f8f7746e28d
marco76tv <marco.sottana@gmail.com>
2025-12-10 11:30:32 +0100

    .

--- app/Filament/Forms/Components/TranslationEditor.php
+++ app/Filament/Forms/Components/TranslationEditor.php
@@ -18,7 +18,7 @@ class TranslationEditor extends Field
     {
         parent::setUp();
 
-        $this->afterStateHydrated(function (TranslationEditor $component, $state): void {
+        $this->afterStateHydrated(function (TranslationEditor $component, $state) {
             $component->state($state ?? []);
         });
     }
