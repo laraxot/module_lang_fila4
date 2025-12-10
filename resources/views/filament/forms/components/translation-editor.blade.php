ee29791d169a156225d5cb2000386636ef56cbd4
marco76tv <marco.sottana@gmail.com>
2025-12-10 17:57:40 +0100

    .

--- resources/views/filament/forms/components/translation-editor.blade.php
+++ resources/views/filament/forms/components/translation-editor.blade.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <?php
 
 declare(strict_types=1);
@@ -7,3 +8,9 @@ declare(strict_types=1);
 <div>
     {{ $this->getChildComponents() }}
 </div>
+=======
+<!-- resources/views/filament/components/translation-editor.blade.php -->
+<div>
+    {{ $this->getChildComponents() }}
+</div>
+>>>>>>> 8b0b6ac (.)
