b364ea93e575c0dd6418cef9f5957cef58e3d45f
marco76tv <marco.sottana@gmail.com>
2026-01-08 09:51:42 +0100

    .

--- resources/views/filament/widgets/language-switcher.blade.php
+++ resources/views/filament/widgets/language-switcher.blade.php
@@ -1,8 +1,11 @@
+<<<<<<< HEAD
 <?php
 
 declare(strict_types=1);
 
 ?>
+=======
+>>>>>>> 8b0b6ac (.)
 {{--
 /**
  * Language Switcher Widget
@@ -220,4 +223,8 @@ if (!function_exists('getLanguageUrl')) {
     }
 }
 @endphp
+<<<<<<< HEAD
 </div>
+=======
+</div>
+>>>>>>> 8b0b6ac (.)
