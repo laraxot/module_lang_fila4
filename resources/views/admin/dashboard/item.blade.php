ea178f1ee3734ad7970e448e3519ac195f730246
marco76tv <marco.sottana@gmail.com>
2026-01-08 09:55:28 +0100

    .

--- resources/views/admin/dashboard/item.blade.php
+++ resources/views/admin/dashboard/item.blade.php
@@ -1,16 +1,6 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 1c4a063 (.)
 <?php
 
 declare(strict_types=1);
 
 ?>
 <!-- dashboard item -->
-<<<<<<< HEAD
-=======
-<!-- dashboard item -->
->>>>>>> 8b0b6ac (.)
-=======
->>>>>>> 1c4a063 (.)
