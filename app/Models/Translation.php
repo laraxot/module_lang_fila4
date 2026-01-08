4ea140633b2da5e19a06c97f0859b87c5430da6d
marco76tv <marco.sottana@gmail.com>
2026-01-08 10:02:22 +0100

    .

--- app/Models/Translation.php
+++ app/Models/Translation.php
@@ -103,6 +103,7 @@ class Translation extends BaseModel
      * Get the current connection name for the model.
      *
      * @return string|null
+<<<<<<< HEAD
      *
      * public function getConnectionName()
      * {
@@ -113,4 +114,16 @@ class Translation extends BaseModel
      * return parent::getConnectionName();
      * }
      */
+=======
+
+    public function getConnectionName()
+    {
+        if ($connection = config('translation-manager.db_connection')) {
+            return $connection;
+        }
+
+        return parent::getConnectionName();
+    }
+    */
+>>>>>>> 8b0b6ac (.)
 }
