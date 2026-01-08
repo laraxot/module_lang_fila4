96bdc46559669bb43392ecf70f502fd0d4fd7668
marco76tv <marco.sottana@gmail.com>
2026-01-08 10:00:54 +0100

    .

--- app/Filament/Forms/Components/NationalFlagSelect.php
+++ app/Filament/Forms/Components/NationalFlagSelect.php
@@ -2,11 +2,7 @@
 
 declare(strict_types=1);
 
-<<<<<<< HEAD
 namespace Modules\Notify\Filament\Forms\Components;
-=======
-namespace Modules\Lang\Filament\Forms\Components;
->>>>>>> f5327e8 (.)
 
 use Illuminate\Support\Arr;
 use Modules\Xot\Actions\File\AssetAction;
