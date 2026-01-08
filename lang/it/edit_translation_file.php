daa18aced1f0df8951f247a5de6e89cd613f4fa8
marco76tv <marco.sottana@gmail.com>
2026-01-08 09:58:47 +0100

    .

--- lang/it/edit_translation_file.php
+++ lang/it/edit_translation_file.php
@@ -1,6 +1,9 @@
 <?php
 
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 1c4a063 (.)
 declare(strict_types=1);
 
 return [
@@ -411,6 +414,7 @@ return [
         ],
     ],
 ];
+<<<<<<< HEAD
 =======
 return array (
   'fields' => 
@@ -926,3 +930,5 @@ return array (
   ),
 );
 >>>>>>> 8b0b6ac (.)
+=======
+>>>>>>> 1c4a063 (.)
